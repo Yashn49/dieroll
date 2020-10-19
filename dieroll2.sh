00b21a52ec8f5aaf40ba2182a90d9d0437f3411f
Yash <yashn49@gmail.com>
2020-10-19 12:01:24 +0530

    my first commit

---
+++ dieroll2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+count1=0
+count2=0
+sum=0
+while [ true ]
+do
+ dieroll1=$(((RANDOM%6)+1))
+ dieroll2=$(((RANDOM%6)+1))
+ count1=$(($count1+1))
+ count2=$(($count2+1))
+ total=$(($sum+$dieroll1+$dieroll2))
+ if [ $dieroll1 -eq 6 -a $dieroll2 -eq 6 ]
+ then
+ echo "same"
+ break;
+ fi
+done
+echo "dice1 got 6 $count1 times"
+echo "dice2 got 6 $count2 times"
+echo "the sum of both dices: " $sum
+
